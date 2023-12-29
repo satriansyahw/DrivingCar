26ca0cf3a997cd021e0d2a76064d8746943f8c8b
Satriansyah <satriansyahw@gmail.com>
2023-12-29 15:57:16 +0800

    add implementation for Services - DrivingCarService

---
+++ src/DrivingCar.Domain/Services/DrivingCarService.cs
@@ -0,0 +1,32 @@
+﻿using DrivingCar.Domain.Commands;
+using DrivingCar.Domain.Shared;
+
+namespace DrivingCar.Domain.Services
+{
+    public class DrivingCarService
+    {
+        public void ExecuteCommands(Car car, Field field, string commands)
+        {
+            foreach (char command in commands)
+            {
+                ICommand commandObj;
+                switch (command)
+                {
+                    case 'L':
+                        commandObj = new LeftCommand();
+                        break;
+                    case 'R':
+                        commandObj = new RightCommand();
+                        break;
+                    case 'F':
+                        commandObj = new ForwardCommand();
+                        break;
+                    default:
+                        throw new ArgumentException($"Invalid command: {command}");
+                }
+
+                commandObj.Execute(car, field);
+            }
+        }
+    }
+}
