bce226c2eafa114d8a56e021297341d68a283c23
Satriansyah <satriansyahw@gmail.com>
2023-12-29 15:27:37 +0800

    add Shared classes : Car & Field

---
+++ src/DrivingCar.Domain/Shared/Car.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrivingCar.Domain.Shared
+{
+    public class Car
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public char Direction { get; set; }
+    }
+}
---
+++ src/DrivingCar.Domain/Shared/Field.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrivingCar.Domain.Shared
+{
+    public class Field
+    {
+        public int Width { get; }
+        public int Height { get; }
+        public Field(int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
+    }
+}
